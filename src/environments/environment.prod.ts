b0cd351c8ede58adafdd6ef275d8a65fe1479063
E Iván Mtz Leal <eivanmtzleal@MacBook-Pro.local>
2020-10-31 21:04:43 -0600

    Add text in README, gitIgoner enviroments

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,13 +1,13 @@
 export const environment = {
   production: true,
   firebaseConfig: {
-    apiKey: 'AIzaSyDhbzhwCaqpxp1VObFnPJ8a4bTyGyTPR-Q',
-    authDomain: 'covid19mx-1bf27.firebaseapp.com',
-    databaseURL: 'https://covid19mx-1bf27.firebaseio.com',
-    projectId: 'covid19mx-1bf27',
-    storageBucket: 'covid19mx-1bf27.appspot.com',
-    messagingSenderId: '148119478481',
-    appId: '1:148119478481:web:f1e73fdf0095c5465865f0',
-    measurementId: 'G-H1PPCHSJDQ'
-  }
+    apiKey: "AIzaSyDhbzhwCaqpxp1VObFnPJ8a4bTyGyTPR-Q",
+    authDomain: "covid19mx-1bf27.firebaseapp.com",
+    databaseURL: "https://covid19mx-1bf27.firebaseio.com",
+    projectId: "covid19mx-1bf27",
+    storageBucket: "covid19mx-1bf27.appspot.com",
+    messagingSenderId: "148119478481",
+    appId: "1:148119478481:web:f1e73fdf0095c5465865f0",
+    measurementId: "G-H1PPCHSJDQ",
+  },
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,19 +1,18 @@
 // This file can be replaced during build by using the `fileReplacements` array.
 // `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
 // The list of file replacements can be found in `angular.json`.
-
 export const environment = {
   production: false,
   firebaseConfig: {
-    apiKey: 'AIzaSyDhbzhwCaqpxp1VObFnPJ8a4bTyGyTPR-Q',
-    authDomain: 'covid19mx-1bf27.firebaseapp.com',
-    databaseURL: 'https://covid19mx-1bf27.firebaseio.com',
-    projectId: 'covid19mx-1bf27',
-    storageBucket: 'covid19mx-1bf27.appspot.com',
-    messagingSenderId: '148119478481',
-    appId: '1:148119478481:web:f1e73fdf0095c5465865f0',
-    measurementId: 'G-H1PPCHSJDQ'
-  }
+    apiKey: "AIzaSyDhbzhwCaqpxp1VObFnPJ8a4bTyGyTPR-Q",
+    authDomain: "covid19mx-1bf27.firebaseapp.com",
+    databaseURL: "https://covid19mx-1bf27.firebaseio.com",
+    projectId: "covid19mx-1bf27",
+    storageBucket: "covid19mx-1bf27.appspot.com",
+    messagingSenderId: "148119478481",
+    appId: "1:148119478481:web:f1e73fdf0095c5465865f0",
+    measurementId: "G-H1PPCHSJDQ",
+  },
 };
 
 /*
